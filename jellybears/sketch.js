df7e118303b0f5ea72627dcad1094cc6817ba20a
Andreas Richard <richard4231@gmail.com>
2020-04-15 21:14:30 +0200

    better distribution

--- jellybears/sketch.js
+++ jellybears/sketch.js
@@ -10,6 +10,8 @@
 var bg = 50;
 var img = [];
 let button;
+let posx = []
+let posy = []
 
 function preload() {
   img[1] = loadImage('assets/orange.png');
@@ -27,7 +29,7 @@ function setup() {
 	createCanvas(500, 500);
 	colorMode(HSB);
 	background(bg);
-	button = createButton('neu laden');
+	button = createButton('neu');
 	button.position(19, 19);
 	button.mousePressed(newPackage);
 	noLoop();
@@ -39,17 +41,36 @@ function draw() {
 	average = 9
 	num = round(randomGaussian(average,0.5));
 	if (num < average - 2) {
-	num = average - 2;
+		num = average - 2;
+	} else if (num > average + 2) {
+		num = average + 2;
 	}
-	if (num > average + 2) {
-	num = average + 2;
+	posx[0] = random(50,450);
+	posy[0] = random(50,450);
+	
+	for (let i = 1; i < num; i++) {
+		posx[i] = random(50,450);
+		posy[i] = random(50,450);
+		// create array and check for near duplicates
+		counter = 0;
+		while (counter < i) {
+			for (let j = 0; j < i; j++) {
+				if (abs(posx[i]-posx[j])<10 && abs(posy[i]-posy[j])<10){
+					posx[i] = random(50,450);
+					posy[i] = random(50,450);
+				} else {
+					counter = counter + 1;
+				}
+			}
+		}
 	}
 
 	for (let i = 0; i < num; i++) {
 			push();
+			
 			//translate(randomGaussian(250,80),randomGaussian(250,80));
 			// check for similar coordinates
-			translate(random(50,450),random(50,450))
+			translate(posx[i],posy[i])
 			rotate(random()*360);
 			j = random([1,2,3,4,5,6]);
 			image(img[j], 0, 0, 56,82);
