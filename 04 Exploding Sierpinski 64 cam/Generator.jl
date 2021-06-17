057ce16095c832aa061d4d00afe13ea1fdc49fc4
richard4231 <63576016+richard4231@users.noreply.github.com>
2021-06-17 21:13:55 +0200

    Add files via upload

---
+++ 04 Exploding Sierpinski 64 cam/Generator.jl
@@ -0,0 +1,13 @@
+n = 4
+A = [1,2,2,1]
+B = []
+for i in 0:3
+    for j in 0:3
+        if (i+1) < n/2 and (j+1) < n/2
+            a = i + j*n
+            push!(B,copy(A[a])
+        elseif
+    end
+end
+
+#A.push(3) (in P5JS)
