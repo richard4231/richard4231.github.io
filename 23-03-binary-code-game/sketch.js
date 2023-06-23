de50e3945bf94eda921d6d13ccd38ee59906c5c5
richard4231 <63576016+richard4231@users.noreply.github.com>
2023-06-23 11:36:20 +0200

    Button

--- 23-03-binary-code-game/sketch.js
+++ 23-03-binary-code-game/sketch.js
@@ -7,21 +7,24 @@ Code for video https://vimeo.com/channels/learningp5js/141919520
 
 var bubbles = [];
 var sum = 0;
+var sum1 = 0;
 var length = 6;
-var rand = Math.floor(Math.random()*12);
+var randListElement = Math.floor(Math.random()*12); //number to be found
 var list_num = [1,17,19,22,9,8,12,32,63,10,24,16];
+var targetNumber = list_num[randListElement];
+var counter = 0;
+var counter1 = 0;
+var flag = 0;
 
 function setup() {
   createCanvas(600, 600);
   for (var i = 0; i < length; i++) {
-    var x = 50 + i * 60;
+    var x = 70 + i * 60;
     var y = 100;
     bubbles.push(new Bubble(x, y,i));
-    print(rand);
   }
-  checkButton = createButton('Überprüfen');
-  checkButton.position(300, 450);
-  checkButton.mousePressed(checkSolution);
+  button1 = createButton('Überprüfen');
+  button1.position(450, 90);
 }
 
 function mouseClicked() {
@@ -46,26 +49,68 @@ function mouseClicked() {
       sum = sum + pow(2,bubbles.length - 1 - i);
     }
   }
-  print(sum);
+  //print(sum);
   sum = 0;
 }
 
+function check_answer() {
+  for (var i = 0; i < bubbles.length; i++) {
+    if (bubbles[i].c) {
+      sum1 = sum1 + pow(2,bubbles.length - 1 - i);
+    }
+  }
+  print(sum1);
+  print(targetNumber);
+  fill(200);
+  textSize(18);
+  textAlign(LEFT,CENTER);
+  if (sum1 == targetNumber){
+    flag = 1;
+    counter1 = counter1 + 1;
+    if (counter1 == 2) {
+      window.location.reload();
+    }
+  }
+  else if (sum1 != targetNumber){
+    flag = 2;
+    counter = counter + 1;
+    if (counter == 2) {
+      window.location.reload();
+    }
+    
+  }
+  sum1 = 0;
+  print(counter);
+}
+
 function draw() {
-  background(0);
+  background(30);
   for (var i = 0; i < bubbles.length; i++) {
     bubbles[i].display();
   }
   fill(200);
   textSize(60);
   textAlign(CENTER,CENTER);
-  text(list_num[rand],200,320);
+  text(list_num[randListElement],200,320);
   textSize(18);
   textAlign(LEFT,CENTER);
+  
+
   text("Versucht die angegebene Zahl mit Binärcode darzustellen.",30,400);
   text("Klickt dazu abwechslungsweise auf einen Stellenwert.",30,425);
   text("Wie viele Versuche braucht ihr?",30,470);
 
+  if (flag ==1) {
+    text("Korrekt!",450,130);
+    }
+    else if (flag == 2) {
+    text("Leider falsch!",450,130);
+    }
+    else {
+      fill(0); 
+    }
 
+  button1.mousePressed(check_answer);
 }
 
 /////
